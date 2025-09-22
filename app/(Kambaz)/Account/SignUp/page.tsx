b6ed86bb94f8c5bde73d4b5b328d2a19b1b4cfbb
lifeashaze <abhishekbhagawati@gmail.com>
2025-09-22 18:23:18 -0400

    signUp default values

--- app/(Kambaz)/Account/SignUp/page.tsx
+++ app/(Kambaz)/Account/SignUp/page.tsx
@@ -3,14 +3,24 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" />
+      <input
+        placeholder="username"
+        className="wd-username"
+        defaultValue="username"
+      />
       <br />
-      <input placeholder="password" type="password" className="wd-password" />
+      <input
+        placeholder="password"
+        type="password"
+        className="wd-password"
+        defaultValue="password"
+      />
       <br />
       <input
         placeholder="verify password"
         type="password"
         className="wd-password-verify"
+        defaultValue="password"
       />
       <br />
       <Link href="Profile"> Sign up </Link>
