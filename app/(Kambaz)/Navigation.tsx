eb4a7bb8ddafee687cb5b1b7da80bd2f40f9e6a2
lifeashaze <abhishekbhagawati@gmail.com>
2025-09-22 20:29:07 -0400

    add neu monogram to main navigation

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -4,29 +4,49 @@ import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
+import Image from "next/image";
 export default function KambazNavigation() {
- return (
-   <ListGroup className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2" style={{ width: 120 }}
-              id="wd-kambaz-navigation">
-     <ListGroupItem className="bg-black border-0 text-center" as="a"
-              target="_blank" href="https://www.northeastern.edu/" id="wd-neu-link">
-       <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
-     </ListGroupItem><br />
-     <ListGroupItem className="border-0 bg-black text-center">
-       <Link href="/Account" id="wd-account-link" className="text-white text-decoration-none">
-         <FaRegCircleUser className="fs-1 text-white" />
-         <br />
-         Account
-       </Link>
-     </ListGroupItem><br />
-     <ListGroupItem className="border-0 bg-white text-center">
-       <Link href="/Dashboard" id="wd-dashboard-link" className="text-danger text-decoration-none">
-         <AiOutlineDashboard className="fs-1 text-danger" />
-         <br />
-         Dashboard
-       </Link>
-     </ListGroupItem><br />
-     {/* complete styling the rest of the links */}
-   </ListGroup>
-);}
-
+  return (
+    <ListGroup
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      style={{ width: 120 }}
+      id="wd-kambaz-navigation"
+    >
+      <ListGroupItem
+        className="bg-black border-0 text-center"
+        as="a"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        id="wd-neu-link"
+      >
+        <Image src="/neu.png" width={90} height={90} alt={""} />
+      </ListGroupItem>
+      <br />
+      <ListGroupItem className="border-0 bg-black text-center">
+        <Link
+          href="/Account"
+          id="wd-account-link"
+          className="text-white text-decoration-none"
+        >
+          <FaRegCircleUser className="fs-1 text-white" />
+          <br />
+          Account
+        </Link>
+      </ListGroupItem>
+      <br />
+      <ListGroupItem className="border-0 bg-white text-center">
+        <Link
+          href="/Dashboard"
+          id="wd-dashboard-link"
+          className="text-danger text-decoration-none"
+        >
+          <AiOutlineDashboard className="fs-1 text-danger" />
+          <br />
+          Dashboard
+        </Link>
+      </ListGroupItem>
+      <br />
+      {/* complete styling the rest of the links */}
+    </ListGroup>
+  );
+}
