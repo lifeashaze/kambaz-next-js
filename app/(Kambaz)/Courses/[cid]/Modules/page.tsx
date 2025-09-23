5257b907a2c322cd8f37967477756a683496c6c6
lifeashaze <abhishekbhagawati@gmail.com>
2025-09-22 20:15:57 -0400

    setup done for assignment 2

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,146 +1,50 @@
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import ModulesControls from "./ModuleControls";
+import { BsGripVertical } from "react-icons/bs";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
+
 export default function Modules() {
   return (
     <div>
-      <div>
-        <button>Collapse All</button> <button>View Progress</button>{" "}
-        <select name="" id="">
-          <option value="">Publish All</option>
-        </select>{" "}
-        <button>+ Module</button>
-      </div>
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">
-            Week 1, Lecture 1 - Course Introduction, Agenda
+      <ModulesControls />
+      <br />
+      <br />
+      <br />
+      <br />
+      <ListGroup className="rounded-0" id="wd-modules">
+        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 1{" "}
+            <ModuleControlButtons />
           </div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-              </ul>
-            </li>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
+              <LessonControlButtons />
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Introduction to the
+              course <LessonControlButtons />
+            </ListGroupItem>
 
-            <li className="wd-lesson">
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 1 - Introduction
-                </li>
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 2 - Creating User
-                </li>
-              </ul>
-            </li>
-
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Introduction to Web Development
-                </li>
-                <li className="wd-content-item">
-                  Creating a HTTP Server with Node.js
-                </li>
-                <li className="wd-content-item">
-                  Creating a React Application
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-
-        <li className="wd-module">
-          <div className="wd-title">
-            Week 1, Lecture 2 - W1L2 Title
-          </div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Example Content Item 1</li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-              </ul>
-            </li>
-
-            <li className="wd-lesson">
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                 Example Content Item 1
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-              </ul>
-            </li>
-
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Example Content Item 1
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 3
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-
-        <li className="wd-module">
-          <div className="wd-title">
-            Week 2, Lecture 1 - W2L1 Title
-          </div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Example Content Item 1</li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-              </ul>
-            </li>
-
-            <li className="wd-lesson">
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Example Content Item 1
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-              </ul>
-            </li>
-
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Example Content Item 1
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 2
-                </li>
-                <li className="wd-content-item">
-                  Example Content Item 3
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              Learn what is Web Development{" "}
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              LESSON 1{" "}
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              LESSON 2{" "}
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+      </ListGroup>
     </div>
   );
 }
