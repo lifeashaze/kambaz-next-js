77b64254245573844a90ee65df4609a3f158b58e
lifeashaze <abhishekbhagawati@gmail.com>
2025-10-06 15:15:43 -0400

    buttons for courses home and additionals modules added

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -6,13 +6,10 @@ import ModuleControlButtons from "./ModuleControlButtons";
 
 export default function Modules() {
   return (
-    <div>
+    <div className="d-flex flex-column gap-5">
       <ModulesControls />
-      <br />
-      <br />
-      <br />
-      <br />
       <ListGroup className="rounded-0" id="wd-modules">
+
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             <BsGripVertical className="me-2 fs-3" /> Week 1{" "}
@@ -27,20 +24,55 @@ export default function Modules() {
               <BsGripVertical className="me-2 fs-3" /> Introduction to the
               course <LessonControlButtons />
             </ListGroupItem>
-
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              Learn what is Web Development{" "}
+              <BsGripVertical className="me-2 fs-3" /> Learn what is Web
+              Development <LessonControlButtons />
             </ListGroupItem>
           </ListGroup>
         </ListGroupItem>
+
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 2{" "}
+            <ModuleControlButtons />
+          </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              LESSON 1{" "}
+              <BsGripVertical className="me-2 fs-3" /> HTML Basics: Structure
+              and Tags <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              LESSON 2{" "}
+              <BsGripVertical className="me-2 fs-3" /> Creating Lists, Links,
+              and Images <LessonControlButtons />
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Building Your First Web
+              Page <LessonControlButtons />
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+
+        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 3{" "}
+            <ModuleControlButtons />
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Introduction to CSS{" "}
+              <LessonControlButtons />
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Styling Text, Colors, and
+              Layouts <LessonControlButtons />
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Understanding the Box
+              Model <LessonControlButtons />
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Hands-on: Recreate a
+              Simple Landing Page <LessonControlButtons />
             </ListGroupItem>
           </ListGroup>
         </ListGroupItem>
