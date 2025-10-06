65c6e45ae7a2268a601ac759ccaf94ab44997728
lifeashaze <abhishekbhagawati@gmail.com>
2025-10-05 23:20:09 -0400

    update

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -5,10 +5,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { BiBook, BiCalendar } from "react-icons/bi";
 import { PiPrinter } from "react-icons/pi";
+
 export default function KambazNavigation() {
   return (
     <ListGroup
-      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-flex flex-column align-items-center justify-content-start bg-black z-2 py-3 gap-4"
       style={{ width: 120 }}
       id="wd-kambaz-navigation"
     >
@@ -19,94 +20,74 @@ export default function KambazNavigation() {
         href="https://www.northeastern.edu/"
         id="wd-neu-link"
       >
-        <Image src="/neu.png" width={90} height={90} alt={""} />
+        <Image src="/neu.png" width={90} height={90} alt="Northeastern University" />
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-black text-center">
         <Link
           href="/Account"
           id="wd-account-link"
-          className="text-white text-decoration-none"
+          className="text-white text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <FaRegCircleUser className="fs-1 text-white" />
-          <br />
-          Account
+          <span>Account</span>
         </Link>
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-white text-center">
         <Link
           href="/Dashboard"
           id="wd-dashboard-link"
-          className="text-danger text-decoration-none"
+          className="text-danger text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <AiOutlineDashboard className="fs-1 text-danger" />
-          <br />
-          Dashboard
+          <span>Dashboard</span>
         </Link>
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-black text-center">
         <Link
           href="/Courses/1234"
           id="wd-courses-link"
-          className="text-white text-decoration-none"
+          className="text-white text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <BiBook className="fs-1 text-danger" />
-          <br />
-          Courses
+          <span>Courses</span>
         </Link>
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-black text-center">
         <Link
           href="/Calendar"
           id="wd-calendar-link"
-          className="text-white text-decoration-none"
+          className="text-white text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <BiCalendar className="fs-1 text-danger" />
-          <br />
-          Calendar
+          <span>Calendar</span>
         </Link>
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-black text-center">
         <Link
           href="/Inbox"
           id="wd-inbox-link"
-          className="text-white text-decoration-none"
+          className="text-white text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <PiPrinter className="fs-1 text-danger" />
-          <br />
-          Inbox
+          <span>Inbox</span>
         </Link>
       </ListGroupItem>
-
-      <br />
 
       <ListGroupItem className="border-0 bg-black text-center">
         <Link
           href="/Labs"
-          id="wd-courses-link"
-          className="text-white text-decoration-none"
+          id="wd-labs-link"
+          className="text-white text-decoration-none d-flex flex-column align-items-center gap-1"
         >
           <FaComputer className="fs-1 text-danger" />
-          <br />
-          Labs
+          <span>Labs</span>
         </Link>
       </ListGroupItem>
-
-      <br />
     </ListGroup>
   );
 }
